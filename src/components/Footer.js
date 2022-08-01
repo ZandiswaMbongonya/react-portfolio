4b3e8b30618ffaeab0689b9d993b52d1ec4aa306
ZandiswaMbongonya <mbongonyazg@gmail.com>
2022-08-01 21:34:32 +0200

    add
    add

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./footer.css";
+const Footer = () => {
+  return (
+    <footer>
+    <div class="container">
+        <div class="footer-text">
+    <p>
+      **This is my portfolio. The given projects belong to me.
+    </p>
+    <p>
+      © Created for
+      <a href="https://github.com/ZandiswaMbongonya/" target="_blank">ZandiswaMbongonya <i class="fa fa-free-code-camp"></i></a>
+    </p>
+</div>
+</div>
+  </footer>
+  )
+}
+
+export default Footer
